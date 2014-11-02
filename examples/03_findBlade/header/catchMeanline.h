70d67064de3872169cbf612b746eb9dab7fe81b8
Alexander Tismer <atismer@ringkolbenschieber.ihs.uni-stuttgart.de>
2014-11-02 15:15:57 +0100

    *add opti example

---
+++ examples/03_findBlade/header/catchMeanline.h
@@ -0,0 +1,10 @@
+#include <vector>
+
+#ifndef CATCHMEANLINE_H
+#define CATCHMEANLINE_H
+
+
+//std::vector<double> _ind1;
+std::vector<double> catchMeanline (int argc, char **argv);
+
+#endif
---
+++ examples/03_findBlade/header/ffMeanline.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   fitnessfunct.h
+ * Author: mjanssen
+ *
+ * Created on 6. August 2014, 14:26
+ */
+
+
+#ifndef FFMEANLINE_H
+#define	FFMEANLINE_H
+
+double ffMeanline(const std::vector<double>& _ind);
+
+#endif	/* FITNESSFUNCT_H */
+
