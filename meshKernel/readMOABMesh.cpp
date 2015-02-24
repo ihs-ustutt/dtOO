a10f67ee723925265090c9f7ed300c20b021db6a
Alexander Tismer <atismer@ringkolbenschieber.ihs.uni-stuttgart.de>
2015-02-24 10:20:28 +0100

    *add some outout to reading

--- meshKernel/readMOABMesh.cpp
+++ meshKernel/readMOABMesh.cpp
@@ -43,8 +43,11 @@ namespace dtOO {
 		dt__THROW_IF(_mb == NULL, makeGrid());
 
 		// Load the mesh from vtk file
-		moab::ErrorCode rval = _mb->load_mesh(_fileName.c_str());
-		dt__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());
+		moab::ErrorCode rval;
+		rval = _mb->delete_mesh();
+		moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
+		rval = _mb->load_mesh(_fileName.c_str());
+		moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
 	}
   
 	void readMOABMesh::makePreGrid(void) {
@@ -63,7 +66,7 @@ namespace dtOO {
 		std::vector<double> yy(verts.size());
 		std::vector<double> zz(verts.size());
 		rval = _mb->get_coords(verts, &xx[0], &yy[0], &zz[0]);
-		dt__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());
+		moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());
 		
 		unstructured3dMesh * um = new unstructured3dMesh();
 		rV[0] = um;
@@ -73,13 +76,13 @@ namespace dtOO {
 		
 	  moab::Range hex;
     rval = _mb->get_entities_by_type(0, moab::MBHEX, hex);
-		dt__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());
+		moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());
 		
 		for (moab::Range::iterator it = hex.begin(); it != hex.end(); it++) {
 			moab::EntityHandle const * conn;
 			int nNodes;
 			rval = _mb->get_connectivity(*it, conn, nNodes);
-			dt__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());
 
 //   		DTINFOWF(
 //				makeGrid(), 
@@ -95,29 +98,61 @@ namespace dtOO {
 	
 		int nMeshSets;
 		rval = _mb->num_contained_meshsets(0, &nMeshSets, 0);
-		moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());
+		moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());
 		DTINFOWF( getRender(), << DTLOGEVAL(nMeshSets) );			
 		
 		moab::Range allSets;
     rval = _mb->get_entities_by_type(0, moab::MBENTITYSET, allSets);
-		moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
+		moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());		
 		for (moab::Range::iterator it=allSets.begin(); it!=allSets.end(); it++) {
 		  moab::EntityHandle currentSet = *it;
 			int dim1; int dim2; int dim3;
 			rval = _mb->get_number_entities_by_dimension(currentSet, 1, dim1, true);
-			moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());		
 			rval = _mb->get_number_entities_by_dimension(currentSet, 2, dim2, true);
-			moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());		
 			rval = _mb->get_number_entities_by_dimension(currentSet, 3, dim3, true);
-			moab__THROW_IF(rval != moab::MB_SUCCESS, makeGrid());		
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());
 			DTINFOWF(
 				getRender(), 
-				<< "In loop" << LOGDEL
 				<< DTLOGEVAL(dim1) << LOGDEL
 				<< DTLOGEVAL(dim2) << LOGDEL
 				<< DTLOGEVAL(dim3)
 			); 
 		} 		
+		
+		std::vector< moab::Tag > tag_handles;
+		_mb->tag_get_tags(tag_handles);
+		dt__FORALLITER(std::vector< moab::Tag >, tag_handles, it) {
+			moab::Tag currentTag = *it;
+			std::string tagName;
+			rval = _mb->tag_get_name(currentTag, tagName);
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());				
+
+			moab::Range quadEnts;
+			rval
+			=
+			_mb->get_entities_by_type_and_tag(
+				0, moab::MBQUAD, &currentTag, 0, 1, quadEnts
+			);			
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());	
+			
+			moab::Range setEnts;
+			rval
+			=
+			_mb->get_entities_by_type_and_tag(
+				0, moab::MBENTITYSET, &currentTag, 0, 1, setEnts
+			);			
+			moab__THROW_IF(rval != moab::MB_SUCCESS, getRender());	
+			
+			DTINFOWF(
+				getRender(), 
+				<< logMe::dtFormat("%s has %i of type moab::MBENTITYSET.")
+			    % tagName % setEnts.size() << LOGDEL
+				<< logMe::dtFormat("%s has %i of type moab::MBQUAD.")
+			    % tagName % quadEnts.size() << LOGDEL							
+			); 
+		}
 		
 		return rV;
 	}	
