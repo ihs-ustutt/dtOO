1af606c01eb5d2599e13fdd066ccb316e96f4b0c
Alexander Tismer <atismer@ringkolbenschieber.ihs.uni-stuttgart.de>
2015-03-02 11:13:34 +0100

    *add marginFaces

--- meshKernel/map3dTo3dWithInternalGmsh.cpp
+++ meshKernel/map3dTo3dWithInternalGmsh.cpp
@@ -1,4 +1,5 @@
 #include "map3dTo3dWithInternalGmsh.h"
+#include "meshEngine/dtGmshEdge.h"
 #include <analyticGeometryHeaven/analyticGeometryCompound.h>
 #include <meshEngine/dtGmshRegion.h>
 #include <meshEngine/dtGmshModel.h>
@@ -9,6 +10,7 @@
 #include <analyticGeometryHeaven/map3dTo3d.h>
 #include <analyticGeometryHeaven/map2dTo3d.h>
 #include <analyticGeometryHeaven/map1dTo3d.h>
+#include <analyticGeometryHeaven/aGBuilder/map2dTo3d_constructMarginFaces.h>
 #include <functionHeaven/analyticFunction.h>
 #include <baseContainerHeaven/baseContainer.h>
 #include <constValueHeaven/constValue.h>
@@ -64,72 +66,93 @@ namespace dtOO {
 		//
 		// set current model
 		//
-		GModel::setCurrent(_gm.get());
+		::GModel::setCurrent(_gm.get());
 
 		dtGmshRegion * gr = _gm->addRegionToGmshModel(_m3d.get());
+		
+		twoDArrayHandling< int > fId(6,0);
+		
+		for (int ii = 0; ii<6; ii++) {
+			dt__pVH(map2dTo3d) margin = map2dTo3d_constructMarginFaces(
+			  _gm->getDtGmshFaceByTag(ii+1)->getMap2dTo3d(), .1
+			).result();
+			
+			dt__FORALLITER(dt__pVH(map2dTo3d), margin, it) {
+				map2dTo3d const & thisMap = *it;
+				fId[ii].push_back(0);
+				_gm->addIfFaceToGmshModel( &thisMap, &(fId[ii].back()) );
+			}
+			for (int jj=1; jj<5; jj++) {
+				_gm->getDtGmshFaceByTag(fId[ii][jj])->meshTransfiniteWNElements(10, 2);
+				_gm->getDtGmshFaceByTag(fId[ii][jj])->meshTransfiniteWNElements(10, 2);
+			}
+		}
 
-		twoDArrayHandling< int > vId(_internal.size(), 4);
-		twoDArrayHandling< int > eId(_internal.size(), 4);
-		dt__FORALLINDEX(_internal, ii) {
-			dtPoint2 p0(0., 0.);
-			dtPoint2 p1(1., 0.);
-			dtPoint2 p2(1., 1.);
-			dtPoint2 p3(0., 1.);
-			
-			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p0), &(vId[ii][0]));
-			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p1), &(vId[ii][1]));
-			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p2), &(vId[ii][2]));
-			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p3), &(vId[ii][3]));
-			
-			
-			_gm->addIfEdgeToGmshModel(
-			  dt__pH(map1dTo3d)(_internal[ii].segmentPercent(p0, p1)).get(),
-				&(eId[ii][0]), vId[ii][0], vId[ii][1]
-			);
-			_gm->addIfEdgeToGmshModel(
-			  dt__pH(map1dTo3d)(_internal[ii].segmentPercent(p1, p2)).get(),
-				&(eId[ii][1]), vId[ii][1], vId[ii][2]
-			);
-			_gm->addIfEdgeToGmshModel(
-			  dt__pH(map1dTo3d)(_internal[ii].segmentPercent(p2, p3)).get(),
-				&(eId[ii][2]), vId[ii][2], vId[ii][3]
-			);
-			_gm->addIfEdgeToGmshModel(
-			  dt__pH(map1dTo3d)(_internal[ii].segmentPercent(p3, p0)).get(),
-				&(eId[ii][3]), vId[ii][3], vId[ii][0]
-			);			
-			DTINFOWF(
-				init(), 
-				<< "vId[ii] = " << vId[ii] << LOGDEL
-				<< "eId[ii] = " << eId[ii]
-			);
-			std::list<GEdge*> ge;
-			std::vector<int> eori(4);						
-			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][0])) );
-			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][1])) );
-			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][2])) );
-			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][3])) );
-			eori[0] = ( eId[ii][0] < 0 ? -1 : 1);
-			eori[1] = ( eId[ii][1] < 0 ? -1 : 1);
-			eori[2] = ( eId[ii][2] < 0 ? -1 : 1);
-			eori[3] = ( eId[ii][3] < 0 ? -1 : 1);			
-			int fId;
-			_gm->addIfFaceToGmshModel( &(_internal[ii]), &fId, ge, eori);
-			_gm->getDtGmshFaceByTag(1)->addEdge(
-        _gm->getEdgeByTag(eId[ii][0]), 1
-			);			
-			_gm->getDtGmshFaceByTag(2)->addEdge(
-        _gm->getEdgeByTag(eId[ii][2]), 1
-			);
-			gr->addFace( _gm->getDtGmshFaceByTag(fId), 1 );
-		}		
+		for (int ii = 0; ii<6; ii++) delete _gm->getDtGmshFaceByTag(ii+1);		
+//		_gm->remove()
+
+//		twoDArrayHandling< int > vId(_internal.size(), 4);
+//		twoDArrayHandling< int > eId(_internal.size(), 4);
+//		dt__FORALLINDEX(_internal, ii) {
+//			dtPoint2 p0(0., 0.);
+//			dtPoint2 p1(1., 0.);
+//			dtPoint2 p2(1., 1.);
+//			dtPoint2 p3(0., 1.);
+//			
+//			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p0), &(vId[ii][0]));
+//			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p1), &(vId[ii][1]));
+//			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p2), &(vId[ii][2]));
+//			_gm->addIfVertexToGmshModel(_internal[ii].getPointPercent(p3), &(vId[ii][3]));
+//			
+//			
+//			_gm->addIfEdgeToGmshModel(
+//			  dt__tmpPtr(map1dTo3d, _internal[ii].segmentPercent(p0, p1)),
+//				&(eId[ii][0]), vId[ii][0], vId[ii][1]
+//			);
+//			_gm->addIfEdgeToGmshModel(
+//			  dt__tmpPtr(map1dTo3d, _internal[ii].segmentPercent(p1, p2)),
+//				&(eId[ii][1]), vId[ii][1], vId[ii][2]
+//			);
+//			_gm->addIfEdgeToGmshModel(
+//			  dt__tmpPtr(map1dTo3d, _internal[ii].segmentPercent(p2, p3)),
+//				&(eId[ii][2]), vId[ii][2], vId[ii][3]
+//			);
+//			_gm->addIfEdgeToGmshModel(
+//			  dt__tmpPtr(map1dTo3d, _internal[ii].segmentPercent(p3, p0)),
+//				&(eId[ii][3]), vId[ii][3], vId[ii][0]
+//			);			
+//			DTINFOWF(
+//				init(), 
+//				<< "vId[ii] = " << vId[ii] << LOGDEL
+//				<< "eId[ii] = " << eId[ii]
+//			);
+//			std::list< ::GEdge * > ge;
+//			std::vector<int> eori(4);						
+//			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][0])) );
+//			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][1])) );
+//			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][2])) );
+//			ge.push_back( _gm->getEdgeByTag(abs(eId[ii][3])) );
+//			eori[0] = ( eId[ii][0] < 0 ? -1 : 1);
+//			eori[1] = ( eId[ii][1] < 0 ? -1 : 1);
+//			eori[2] = ( eId[ii][2] < 0 ? -1 : 1);
+//			eori[3] = ( eId[ii][3] < 0 ? -1 : 1);			
+//			int fId;
+//			_gm->addIfFaceToGmshModel( &(_internal[ii]), &fId, ge, eori);
+//			_gm->getDtGmshFaceByTag(1)->addEdge(
+//        _gm->getEdgeByTag(eId[ii][0]), 1
+//			);			
+//			_gm->getDtGmshFaceByTag(2)->addEdge(
+//        _gm->getEdgeByTag(eId[ii][2]), 1
+//			);
+//			gr->addFace( _gm->getDtGmshFaceByTag(fId), 1 );
+//		}		
 	}
 	
   void map3dTo3dWithInternalGmsh::makeGrid(void) {
 		//
 		// set current model
 		//
-		GModel::setCurrent(_gm.get());
+		::GModel::setCurrent(_gm.get());
 		
     //
     // set options
