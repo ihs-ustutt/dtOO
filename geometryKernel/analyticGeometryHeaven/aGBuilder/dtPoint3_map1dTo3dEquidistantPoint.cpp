a1effc46d1971bc4928883fec2c6fff61a26c61e
atismer <atismer@ringkolbenschieber.ihs.uni-stuttgart.de>
2015-03-03 19:03:34 +0100

    *fix bug

--- geometryKernel/analyticGeometryHeaven/aGBuilder/dtPoint3_map1dTo3dEquidistantPoint.cpp
+++ geometryKernel/analyticGeometryHeaven/aGBuilder/dtPoint3_map1dTo3dEquidistantPoint.cpp
@@ -6,12 +6,12 @@ namespace dtOO {
 	dtPoint3_map1dTo3dEquidistantPoint::dtPoint3_map1dTo3dEquidistantPoint(
     map1dTo3d const * const m1d, int const & nPoints
 	) {
-		std::vector< dtPoint3 > ppXYZ(nPoints);
+		_ppXYZ.resize(nPoints);
 		float dist = 1./(nPoints-1);
 		float length = m1d->length();
 		for (int ii=0; ii<nPoints; ii++) {
 			dt__TOFLOAT(float iiF, ii);
-			_ppXYZ[ii] = m1d->getPointPercent( m1d->u_l(length * dist * iiF) );
+			_ppXYZ[ii] = m1d->getPoint( m1d->u_l(length * dist * iiF) );
 		}
 	}
 
