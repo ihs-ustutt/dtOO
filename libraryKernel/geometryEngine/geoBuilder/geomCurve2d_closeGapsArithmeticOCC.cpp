014c8b59465113d9954abd30c2f7c05440c7e294
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-03-06 15:44:18 +0100

    *add new close arithmetic builder

---
+++ libraryKernel/geometryEngine/geoBuilder/geomCurve2d_closeGapsArithmeticOCC.cpp
@@ -0,0 +1,76 @@
+#include "geomCurve2d_closeGapsArithmeticOCC.h"
+#include <geometryEngine/dtCurve2d.h>
+//#include "geometryEngine/dtOCCCurve2d.h"
+//#include "geometryEngine/dtOCCSurface.h"
+//#include <geometryEngine/dtOCCCurveBase.h>
+//#include <geometryEngine/dtOCCCurve2dBase.h>
+//#include <geometryEngine/dtOCCBSplineCurve.h>
+//#include <geometryEngine/dtOCCBezierCurve.h>
+//#include <geometryEngine/dtOCCBSplineCurve2d.h>
+
+//#include <Geom_Surface.hxx>
+//#include <Geom_Curve.hxx>
+//#include <Geom2d_Curve.hxx>
+//#include <Geom2d_BSplineCurve.hxx>
+//#include <GeomProjLib.hxx>
+
+namespace dtOO {
+	geomCurve2d_closeGapsArithmeticOCC::geomCurve2d_closeGapsArithmeticOCC() {
+		
+	}
+
+	geomCurve2d_closeGapsArithmeticOCC::geomCurve2d_closeGapsArithmeticOCC(
+      dt__pVH(dtCurve2d) const & dtC2d
+	) {
+		
+		typedef std::vector< std::pair< dtVector2, dtVector2 > > pPair;
+		pPair pp;
+		dt__FORALLCONSTITER(dt__pVH(dtCurve2d), dtC2d, it) {
+			dtCurve2d const & theCurve = *it;
+			pp.push_back(
+				std::pair< dtVector2, dtVector2 >(
+					dtLinearAlgebra::toDtVector2(
+			      theCurve.controlPoint(0)
+			    ), 
+					dtLinearAlgebra::toDtVector2(
+			      theCurve.controlPoint(theCurve.nControlPoints()-1)
+			    ) 
+				)
+			);
+		}
+		pp.push_back(pp[0]);
+		
+    dt__FORINNERINDEX(pp, ii) {		
+			pp[ii].first = 0.5 * (pp[ii].first + pp[ii-1].second);
+			pp[ii-1].second = pp[ii].first;
+
+			pp[ii].second = 0.5 * (pp[ii+1].first + pp[ii].second);
+			pp[ii+1].first = pp[ii].second;
+		}
+		
+		pp.front().first = pp.back().first;
+		pp.pop_back();
+		
+		dt__FORALLCONSTITER(dt__pVH(dtCurve2d), dtC2d, it) {
+			int ii = it - dtC2d.begin();
+			dtCurve2d const & theCurve = *it;
+			dtCurve2d * theClone = theCurve.clone();
+			theClone->setControlPoint(
+			  0, dtLinearAlgebra::toDtPoint2(pp[ii].first) 
+			);
+			theClone->setControlPoint( 
+			  theClone->nControlPoints()-1, 
+				dtLinearAlgebra::toDtPoint2(pp[ii].second)
+			);
+			_dtC2d.push_back(theClone);
+		}
+	}
+
+	geomCurve2d_closeGapsArithmeticOCC::~geomCurve2d_closeGapsArithmeticOCC() {
+		
+	}
+	
+	dt__pVH(dtCurve2d) geomCurve2d_closeGapsArithmeticOCC::result( void ) {
+		return _dtC2d;		
+	}
+}
---
+++ libraryKernel/geometryEngine/geoBuilder/geomCurve2d_closeGapsArithmeticOCC.h
@@ -0,0 +1,26 @@
+#ifndef geomCurve2d_closeGapsArithmeticOCC_H
+#define	geomCurve2d_closeGapsArithmeticOCC_H
+
+#include <dtLinearAlgebra.h>
+#include <logMe/dtMacros.h>
+#include <interfaceHeaven/ptrHandling.h>
+#include <interfaceHeaven/vectorHandling.h>
+
+namespace dtOO {
+//  class dtCurve;
+  class dtCurve2d;
+//  class dtSurface;
+  
+  class geomCurve2d_closeGapsArithmeticOCC {
+  public:
+    dt__CLASSNAME(geomCurve2d_closeGapsArithmeticOCC);    
+    geomCurve2d_closeGapsArithmeticOCC();
+    geomCurve2d_closeGapsArithmeticOCC(dt__pVH(dtCurve2d) const & dtC2d);
+    virtual ~geomCurve2d_closeGapsArithmeticOCC();
+    dt__pVH(dtCurve2d) result( void );    
+  private:
+    dt__pVH(dtCurve2d) _dtC2d;
+  };
+}
+#endif	/* geomCurve2d_closeGapsArithmeticOCC_H */
+
