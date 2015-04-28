4c1ac516f5dfe6900083f1d5f232c0026335eb71
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-04-28 10:17:03 +0200

    *improve code structure

--- generalKernel/dtXmlParserDecorator/aFXmlBuilder/sca5PointMeanlineFunctionDecorator.cpp
+++ generalKernel/dtXmlParserDecorator/aFXmlBuilder/sca5PointMeanlineFunctionDecorator.cpp
@@ -42,70 +42,85 @@ namespace dtOO {
     //
     //
 		dt__pH(dtCurve2d) dtC2d;
-    if ( hasAlphaOne && hasAlphaTwo && hasRatio && !hasDeltaX && hasDeltaY && hasOrder && hasPointOne && hasPointTwo) {
+    if (    hasAlphaOne && hasAlphaTwo 
+				 && hasRatio && !hasDeltaX && hasDeltaY 
+				 && hasOrder && hasPointOne && hasPointTwo
+		) {
       //
       // get necessary values
       //
-      float alphaOne = muParseString(
-                         replaceUsedFunctions(
-                           getAttributeStr(
-                             "alpha_one", 
-                             toBuildP
-                           ), 
-                           cValP, 
-                           depSFunP
-                         )
-                       );      
-      float alphaTwo = muParseString(
-                         replaceUsedFunctions(
-                           getAttributeStr(
-                             "alpha_two", 
-                             toBuildP
-                           ), 
-                           cValP, 
-                           depSFunP
-                         )
-                       );
-      float deltaY = muParseString(
-                       replaceUsedFunctions(
-                         getAttributeStr(
-                           "delta_y", 
-                           toBuildP
-                         ), 
-                         cValP, 
-                         depSFunP
-                       )
-                     ); 
-      float ratio = muParseString(
-                       replaceUsedFunctions(
-                         getAttributeStr(
-                           "ratio", 
-                           toBuildP
-                         ), 
-                         cValP, 
-                         depSFunP
-                       )
-                     );   
-       float pointOne = muParseString(
-                       replaceUsedFunctions(
-                         getAttributeStr(
-                           "point_one", 
-                           toBuildP
-                         ), 
-                         cValP, 
-                         depSFunP
-                       )
-                     ); 
-	float pointTwo = muParseString(
-                       replaceUsedFunctions(
-                         getAttributeStr(
-                           "point_two", 
-                           toBuildP
-                         ), 
-                         cValP, 
-                         depSFunP
-                       )
-                     ); 
+      float alphaOne 
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"alpha_one", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			);      
+      float alphaTwo 
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"alpha_two", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			);
+      float deltaY 
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"delta_y", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			); 
+      float ratio
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"ratio", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			);   
+      float pointOne 
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"point_one", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			); 
+	    float pointTwo 
+			= 
+			muParseString(
+				replaceUsedFunctions(
+					getAttributeStr(
+						"point_two", 
+						toBuildP
+					), 
+					cValP, 
+					depSFunP
+				)
+			); 
 			dtC2d.reset(
 				bSplineCurve2d_angleRatioDeltaYPointOnePointTwoConstructOCC(
 					alphaOne, alphaTwo, ratio, deltaY, pointOne, pointTwo
@@ -114,22 +129,25 @@ namespace dtOO {
     }
     
     else {
-      dt__throw(buildPart(),
-              << dt__eval(hasOrder) << std::endl
-              << dt__eval(hasAlphaOne) << std::endl
-              << dt__eval(hasAlphaTwo) << std::endl
-              << dt__eval(hasRatio) << std::endl
-							<< dt__eval(hasDeltaX) << std::endl
-              << dt__eval(hasDeltaY) << std::endl
-              << dt__eval(hasPointOne) << std::endl	       
-              << dt__eval(hasPointTwo) 
-	       );
+      dt__throw(
+				buildPart(),
+				<< dt__eval(hasOrder) << std::endl
+				<< dt__eval(hasAlphaOne) << std::endl
+				<< dt__eval(hasAlphaTwo) << std::endl
+				<< dt__eval(hasRatio) << std::endl
+				<< dt__eval(hasDeltaX) << std::endl
+				<< dt__eval(hasDeltaY) << std::endl
+				<< dt__eval(hasPointOne) << std::endl	       
+				<< dt__eval(hasPointTwo)
+	    );
     }
 		if ( hasAttribute("revert", toBuildP) ) {
 			if ( getAttributeBool("revert", toBuildP) ) {
 				dtOCCBSplineCurve2d::SecureCast(dtC2d.get())->revert();
 			}
-		}		
-		sFunP->push_back( new vec2dCurve2dOneD( dtC2d.get() ) );
+		}
+		sFunP->push_back( 
+		  new vec2dCurve2dOneD( dtC2d.get() ) 
+		);
   }
 }
