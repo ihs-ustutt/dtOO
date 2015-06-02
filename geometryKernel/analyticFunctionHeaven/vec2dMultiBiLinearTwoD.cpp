8dc194bff5af6396a3a08453049f2b8187bf19d7
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-06-02 08:47:06 +0200

    *add output

--- geometryKernel/analyticFunctionHeaven/vec2dMultiBiLinearTwoD.cpp
+++ geometryKernel/analyticFunctionHeaven/vec2dMultiBiLinearTwoD.cpp
@@ -36,9 +36,37 @@ namespace dtOO {
 			setMin( ii, 0. );
 			setMax( ii, 1. );
 		}				
+    
+    twoDArrayHandling< float > rangeY0(_piece.size(0)+1, _piece.size(1)+1);
+    twoDArrayHandling< float > rangeY1(_piece.size(0)+1, _piece.size(1)+1);
+    dt__forAllIndex(_piece, ii) {
+      dt__forAllIndex(_piece[ii], jj) {    
+        aFY y0 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(0., 0.) );
+        aFY y1 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(1., 0.) );
+        aFY y2 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(1., 1.) );
+        aFY y3 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(0., 1.) );
+
+        rangeY0[ii  ][jj  ] = y0[0];
+        rangeY0[ii+1][jj  ] = y1[0];
+        rangeY0[ii+1][jj+1] = y2[0];
+        rangeY0[ii  ][jj+1] = y3[0];
+        rangeY1[ii  ][jj  ] = y0[1];
+        rangeY1[ii+1][jj  ] = y1[1];
+        rangeY1[ii+1][jj+1] = y2[1];
+        rangeY1[ii  ][jj+1] = y3[1];        
+      }
+    }    
+    dt__info(
+      vec2dMultiBiLinearTwoD(),
+      << "y0" << std::endl
+      << logMe::floatMatrixToString(rangeY0) << std::endl
+      << "y1" << std::endl
+      << logMe::floatMatrixToString(rangeY1)
+    );    
   }
  
   vec2dMultiBiLinearTwoD::~vec2dMultiBiLinearTwoD() {
+    _piece.clear();
   }
 
   aFY vec2dMultiBiLinearTwoD::Y( aFX const & xx ) const {
@@ -88,9 +116,34 @@ namespace dtOO {
         }
       } 
     }
+    twoDArrayHandling< float > rangeX0(_piece.size(0)+1, _piece.size(1)+1);
+    twoDArrayHandling< float > rangeX1(_piece.size(0)+1, _piece.size(1)+1);
+    
+    dt__forAllIndex(_piece, ii) {
+      dt__forAllIndex(_piece[ii], jj) {    
+        aFX x0 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(0., 0.) );
+        aFX x1 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(1., 0.) );
+        aFX x2 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(1., 1.) );
+        aFX x3 = _piece[ii][jj].Y( analyticFunction::aFXTwoD(0., 1.) );
+
+        rangeX0[ii  ][jj  ] = x0[0];
+        rangeX0[ii+1][jj  ] = x1[0];
+        rangeX0[ii+1][jj+1] = x2[0];
+        rangeX0[ii  ][jj+1] = x3[0];
+        rangeX1[ii  ][jj  ] = x0[1];
+        rangeX1[ii+1][jj  ] = x1[1];
+        rangeX1[ii+1][jj+1] = x2[1];
+        rangeX1[ii  ][jj+1] = x3[1];        
+      }
+    }
+    
     dt__throw(
       invY(),
       << dt__eval(yy) << std::endl
+      << "rangeX0" << std::endl
+      << logMe::floatMatrixToString(rangeX0) << std::endl
+      << "rangeX1" << std::endl
+      << logMe::floatMatrixToString(rangeX1) << std::endl
       << "Cannot determine interval."
     );
     
