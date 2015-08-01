b34e8e1dc5ff4e4844dcabd2a31eb45cfaa98b40
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-08-01 19:20:16 +0200

    *add plugin example helloWorldPlugin

---
+++ examples/helloWorldPlugin/helloWorldPlugin.cpp
@@ -0,0 +1,75 @@
+#include "helloWorldPlugin.h"
+
+#include <dtLinearAlgebra.h>
+#include <analyticGeometryHeaven/analyticGeometry.h>
+
+namespace dtOO {
+  helloWorldPlugin::helloWorldPlugin() : dtPlugin() {
+    dt__info(
+      helloWorldPlugin(), 
+      << "Create helloWorldPlugin"
+    );
+  }
+
+  helloWorldPlugin::~helloWorldPlugin() {
+    dt__info(
+      helloWorldPlugin(), 
+      << "Destroy helloWorldPlugin"
+    );
+  }
+
+  void helloWorldPlugin::init( 
+    ::QDomElement const & element,
+    baseContainer const * const bC,
+    vectorHandling< constValue * > const * const cV,
+    vectorHandling< analyticFunction * > const * const aF,
+    vectorHandling< analyticGeometry * > const * const aG,
+    vectorHandling< boundedVolume * > const * const bV,
+    vectorHandling< dtPlugin * > const * const pL
+  ) {
+    dtPlugin::init(element, bC, cV, aF, aG, bV, pL);
+
+    dt__info(
+      helloWorldPlugin(), 
+      << "Init helloWorldPlugin"
+    );    
+    
+    //
+    // dump for example a vector
+    //
+    aG->dump();
+  }
+
+  void helloWorldPlugin::apply(void) {
+    dt__info(
+      helloWorldPlugin(), 
+      << "Apply helloWorldPlugin"
+    );          
+  }
+}
+
+//
+// pugg driver
+//
+helloWorldPluginDriver::helloWorldPluginDriver() : dtPluginDriver(
+  std::string("helloWorldPluginDriver"), dtOO::dtPlugin::version
+) {
+
+}
+
+//
+// pugg plugin creation via driver
+//
+dtOO::dtPlugin * helloWorldPluginDriver::create() {
+  return new dtOO::helloWorldPlugin();
+}
+
+//
+// registrate plugin in driver
+//
+#include <pluginEngine/pugg/Kernel.h>
+extern "C" void register_pugg_plugin(pugg::Kernel* kernel) {
+  kernel->add_driver(new helloWorldPluginDriver());
+}
+
+
---
+++ examples/helloWorldPlugin/helloWorldPlugin.h
@@ -0,0 +1,44 @@
+#ifndef helloWorldPlugin_H
+#define	helloWorldPlugin_H
+
+#include <pluginEngine/pugg/Driver.h>
+#include <dtPlugin.h>
+#include <dtPluginDriver.h>
+
+#include <logMe/dtMacros.h>
+#include <interfaceHeaven/vectorHandling.h>
+
+namespace dtOO {
+  class constValue;
+  class analyticFunction;
+  class analyticGeometry;
+  class boundedVolume;
+  class baseContainer;
+  
+  class helloWorldPlugin : public dtPlugin {
+  public:
+    dt__class(helloWorldPlugin, dtPlugin);
+    helloWorldPlugin();
+    virtual ~helloWorldPlugin();
+    virtual void init( 
+      ::QDomElement const & element,
+      baseContainer const * const bC,
+      vectorHandling< constValue * > const * const cV,
+      vectorHandling< analyticFunction * > const * const aF,
+      vectorHandling< analyticGeometry * > const * const aG,
+      vectorHandling< boundedVolume * > const * const bV,
+      vectorHandling< dtPlugin * > const * const pL
+    );
+    
+    virtual void apply(void);
+  private:
+  };
+}
+
+class helloWorldPluginDriver : public dtOO::dtPluginDriver {
+public:
+    helloWorldPluginDriver();
+    dtOO::dtPlugin * create();
+};
+
+#endif	/* helloWorldPlugin_H */
