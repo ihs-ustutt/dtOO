17005024a169bce423a71af7808e628ffaff23f6
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-12-04 15:32:42 +0100

    *via option to decide between pointInterpolate and normal construction

--- generalKernel/xmlHeaven/aFXmlBuilder/vec3dCurveOneDAFXmlBuilder.cpp
+++ generalKernel/xmlHeaven/aFXmlBuilder/vec3dCurveOneDAFXmlBuilder.cpp
@@ -1,8 +1,10 @@
 #include "vec3dCurveOneDAFXmlBuilder.h"
 
 #include <xmlHeaven/dtXmlParserBase.h>
+#include <xmlHeaven/dtXmlParser.h>
 #include <analyticFunctionHeaven/analyticFunction.h>
 #include <geometryEngine/geoBuilder/bSplineCurve_pointConstructOCC.h>
+#include <geometryEngine/geoBuilder/bSplineCurve_pointInterpolateConstructOCC.h>
 #include <geometryEngine/dtCurve.h>
 #include <analyticFunctionHeaven/vec3dCurveOneD.h>
 #include <logMe/logMe.h>
@@ -27,48 +29,74 @@ namespace dtOO {
 		vectorHandling< analyticFunction * > const * const depSFunP,
 		vectorHandling< analyticFunction * > * sFunP 
 	) const {
-    //
-    //check input
-    //
-    bool hasOrder = dtXmlParserBase::hasAttribute("order", toBuildP);
-    bool hasPoints = dtXmlParserBase::hasChild("Point_3", toBuildP);
-    bool hasScaFunction = dtXmlParserBase::hasChild("function", toBuildP);
-    
-
-    if ( hasPoints && hasOrder ) {
-			std::vector< dtPoint3 > pointsArray;
-      ::QDomElement elementP = dtXmlParserBase::getChild("Point_3", toBuildP);
+    dt__throwIf(!dtXmlParserBase::hasChild("Point_3", toBuildP), buildPart());    
       //
-      //set input
+      // get points
       //
-      int order = dtXmlParserBase::getAttributeInt("order", toBuildP);
-      while ( !elementP.isNull() ) {
-				std::vector< dtPoint3 > workingPoint;
-        dtXmlParserBase::createBasic( &elementP, bC, cValP, depSFunP, &workingPoint );
-        for (int ii=0;ii<workingPoint.size();ii++) {
-          // put in point builder
-					pointsArray.push_back( workingPoint[ii] );
-					//delete workingPoint[ii];
-        }
-      // next sibling
-      elementP = ::QDomElement( dtXmlParserBase::getNextSibling("Point_3", elementP) );      
+      std::vector< dtPoint3 > points;
+      dt__forAllRefAuto(
+        dtXmlParserBase::getChildVector("Point_3", toBuildP), anEl
+      ) {
+        std::vector< dtPoint3 > wP;
+        dtXmlParserBase::dtXmlParserBase::createBasic( 
+          &anEl, bC, cValP, depSFunP, &wP 
+        );
+        dt__forAllRefAuto(wP, aWP) points.push_back( aWP );
       }
-			ptrHandling<dtCurve> dtC( 
-				bSplineCurve_pointConstructOCC(pointsArray, order).result() 
-			);
+    
+      //
+      // construct by control points
+      //
+      ptrHandling<dtCurve> dtC;
+      if ( 
+        ( dtXmlParser::getAttributeStr("via", toBuildP, "") == "" )
+        ||
+        ( 
+          dtXmlParser::getAttributeStr("via", toBuildP, "") 
+          == 
+          "pointConstruct" 
+        )
+      ) {
+        dt__throwIf( 
+          !dtXmlParserBase::hasAttribute("order", toBuildP), buildPart() 
+        );
+        dtC.reset( 
+          bSplineCurve_pointConstructOCC(
+            points, 
+            dtXmlParserBase::getAttributeInt("order", toBuildP)
+          ).result() 
+        );
+      }
+      //
+      // construct by pointInterpolate
+      //
+      else if (
+        dtXmlParser::getAttributeStr("via", toBuildP, "") 
+        == 
+        "pointInterpolateConstruct" 
+      ) {
+        dt__throwIf( 
+          dtXmlParserBase::hasAttribute("order", toBuildP), buildPart() 
+        );
+        dt__throwIf( 
+          !dtXmlParserBase::hasAttribute("orderMin", toBuildP), buildPart() 
+        );        
+        dt__throwIf( 
+          !dtXmlParserBase::hasAttribute("orderMax", toBuildP), buildPart() 
+        );                
+        dtC.reset( 
+          bSplineCurve_pointInterpolateConstructOCC(
+            points,
+            dtXmlParserBase::getAttributeInt("orderMin", toBuildP),
+            dtXmlParserBase::getAttributeInt("orderMax", toBuildP)          
+          ).result() 
+        );        
+      }
+      else dt__throwUnexpected(buildPart());
 
       //
-      // create scaCurve2dOneD
+      // create vec3dCurveOneD
       //
       sFunP->push_back( new vec3dCurveOneD( dtC.get() ) );			
-    }
-    else {
-      dt__throw(
-				buildPart(),
-				<< dt__eval(hasOrder) << std::endl
-				<< dt__eval(hasPoints) << std::endl
-				<< dt__eval(hasScaFunction) 
-			);
-    }
   }
 }
