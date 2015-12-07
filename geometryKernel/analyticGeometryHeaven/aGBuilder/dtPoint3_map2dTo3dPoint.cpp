63c92dda772b1005172b86fa3b43c32005752c53
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-12-07 18:16:31 +0100

    *new aGBuilder

---
+++ geometryKernel/analyticGeometryHeaven/aGBuilder/dtPoint3_map2dTo3dPoint.cpp
@@ -0,0 +1,30 @@
+#include "dtPoint3_map2dTo3dPoint.h"
+
+#include <progHelper.h>
+#include <analyticGeometryHeaven/map2dTo3d.h>
+
+namespace dtOO {
+  dtPoint3_map2dTo3dPoint::dtPoint3_map2dTo3dPoint(
+      map2dTo3d const * const m2d, int const & nU, int const & nV
+  ) {
+		_pp.resize(nU, nV);
+
+    twoDArrayHandling< dtPoint2 > uGrid = dtLinearAlgebra::unitGrid(nU, nV);
+		dt__forAllIndex(uGrid, ii) {
+      dt__forAllIndex(uGrid[ii], jj) {
+			  _pp[ii][jj] 
+        = 
+        m2d->getPointPercent( uGrid[ii][jj] );
+      }
+		}				
+	}
+  
+	dtPoint3_map2dTo3dPoint::~dtPoint3_map2dTo3dPoint() {
+		
+	}
+	
+	twoDArrayHandling< dtPoint3 > dtPoint3_map2dTo3dPoint::result( void ) {
+		return _pp;
+	}
+}
+
---
+++ geometryKernel/analyticGeometryHeaven/aGBuilder/dtPoint3_map2dTo3dPoint.h
@@ -0,0 +1,24 @@
+#ifndef dtPoint3_map2dTo3dPoint_H
+#define	dtPoint3_map2dTo3dPoint_H
+
+#include <logMe/dtMacros.h>
+#include <dtLinearAlgebra.h>
+#include <interfaceHeaven/twoDArrayHandling.h>
+
+namespace dtOO {
+  class map2dTo3d;
+  
+  class dtPoint3_map2dTo3dPoint {
+  public:
+    dt__classOnlyName(dtPoint3_map2dTo3dPoint);    
+    dtPoint3_map2dTo3dPoint(
+      map2dTo3d const * const m2d, int const & nU, int const & nV
+    );
+    virtual ~dtPoint3_map2dTo3dPoint();
+    twoDArrayHandling< dtPoint3 > result( void );
+  private:
+    twoDArrayHandling< dtPoint3 > _pp;
+  };
+}
+#endif	/* dtPoint3_map2dTo3dPoint_H */
+
