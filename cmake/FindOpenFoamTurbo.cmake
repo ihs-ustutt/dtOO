9540e93b393eb5b47b32341fbb23f30d403b47d9
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2015-12-08 08:37:30 +0100

    *add OpenFoamTurbo lib *code cleaning

---
+++ cmake/FindOpenFoamTurbo.cmake
@@ -0,0 +1,7 @@
+DTOO_FIND_LIBRARY_EXT(
+  "OpenFoamTurbo_FOUND" "profile1DfixedValueFvPatchField.H" "libOpenFoamTurbo.so" 
+  "OpenFoamTurbo_INCLUDE_DIR" "OpenFoamTurbo_LINK_DIRECTORY" "OpenFoamTurbo_LIBRARIES" 
+  "OpenFoamTurbo_FIND_COMPONENTS"
+  "$ENV{FOAM_INST_DIR}/site/openfoam-extend-Breeder1.6-OSIG-TurboMachinery/src/OpenFoamTurbo/lnInclude" 
+  "$ENV{FOAM_SITE_LIBBIN}"
+)
