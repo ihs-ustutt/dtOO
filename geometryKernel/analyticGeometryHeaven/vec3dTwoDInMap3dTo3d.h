21fe3abb3a826502296b133ee3ee0b7dd194609c
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2016-02-25 10:15:19 +0100

    *extRender already on map2dTo3d

--- geometryKernel/analyticGeometryHeaven/vec3dTwoDInMap3dTo3d.h
+++ geometryKernel/analyticGeometryHeaven/vec3dTwoDInMap3dTo3d.h
@@ -14,33 +14,32 @@ namespace dtOO {
   class dtTransformer;
   
   class vec3dTwoDInMap3dTo3d : public map2dTo3d {
-  public:
-    dt__class(vec3dTwoDInMap3dTo3d, analyticGeometry);
-    vec3dTwoDInMap3dTo3d();
-    vec3dTwoDInMap3dTo3d(const vec3dTwoDInMap3dTo3d& orig);
-    vec3dTwoDInMap3dTo3d(    
-	    vec3dTwoD const * const v2d, 
-		  map3dTo3d const * const m3d, 
-		  bool percentF = false
-    );    
-    virtual ~vec3dTwoDInMap3dTo3d();
-    virtual vec3dTwoDInMap3dTo3d * clone( void ) const;
-    virtual vec3dTwoDInMap3dTo3d * create( void ) const;
-    virtual vec3dTwoDInMap3dTo3d * cloneTransformed( 
-      dtTransformer const * const dtT 
-    ) const;
-    virtual dtPoint3 getPoint( float const & uu, float const & vv ) const;
-    virtual bool isClosed( int const & dir) const;
-    virtual float getMin( int const & dir) const;
-    virtual float getMax( int const & dir) const;
-    vec3dTwoD const * refToVec3dTwoD( void ) const;
-    map3dTo3d const * refToMap3dTo3d( void ) const;
-    vectorHandling< renderInterface * > getExtRender( void ) const;
-    virtual std::string dumpToString( void ) const;
-  private:
-    ptrHandling< vec3dTwoD > _v2d;
-    ptrHandling< map3dTo3d > _m3d;
-    bool _percentF;    
+    public:
+      dt__class(vec3dTwoDInMap3dTo3d, analyticGeometry);
+      vec3dTwoDInMap3dTo3d();
+      vec3dTwoDInMap3dTo3d(const vec3dTwoDInMap3dTo3d& orig);
+      vec3dTwoDInMap3dTo3d(    
+        vec3dTwoD const * const v2d, 
+        map3dTo3d const * const m3d, 
+        bool percentF = false
+      );    
+      virtual ~vec3dTwoDInMap3dTo3d();
+      virtual vec3dTwoDInMap3dTo3d * clone( void ) const;
+      virtual vec3dTwoDInMap3dTo3d * create( void ) const;
+      virtual vec3dTwoDInMap3dTo3d * cloneTransformed( 
+        dtTransformer const * const dtT 
+      ) const;
+      virtual dtPoint3 getPoint( float const & uu, float const & vv ) const;
+      virtual bool isClosed( int const & dir) const;
+      virtual float getMin( int const & dir) const;
+      virtual float getMax( int const & dir) const;
+      vec3dTwoD const * refToVec3dTwoD( void ) const;
+      map3dTo3d const * refToMap3dTo3d( void ) const;
+      virtual std::string dumpToString( void ) const;
+    private:
+      ptrHandling< vec3dTwoD > _v2d;
+      ptrHandling< map3dTo3d > _m3d;
+      bool _percentF;    
   };
 }
 #endif	/* vec3dTwoDInMap3dTo3d_H */
