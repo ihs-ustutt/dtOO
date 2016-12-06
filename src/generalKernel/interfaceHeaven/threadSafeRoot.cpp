55dba761ede6bad52e3adc4d1ebf5c89201495aa
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2016-12-06 18:17:56 +0100

    *reduce output

--- src/generalKernel/interfaceHeaven/threadSafeRoot.cpp
+++ src/generalKernel/interfaceHeaven/threadSafeRoot.cpp
@@ -1,12 +1,23 @@
 #include "threadSafeRoot.h"
 
 #include <logMe/logMe.h>
+#include <interfaceHeaven/staticPropertiesHandler.h>
 #include <omp.h>
 #include <TThread.h>
+#include <TError.h>
 
 namespace dtOO {
   threadSafeRoot::threadSafeRoot() {
-    if (omp_in_parallel() ) _thread.reset( new ::TThread() );
+    if ( omp_in_parallel() ) {
+      _thread.reset( new ::TThread() );
+      if ( 
+        staticPropertiesHandler::getInstance()->getOptionInt("root_printLevel") 
+        ==
+        0
+      ) {
+        gErrorIgnoreLevel = 1001;
+      }
+    }
   }
 
   threadSafeRoot::~threadSafeRoot() {
