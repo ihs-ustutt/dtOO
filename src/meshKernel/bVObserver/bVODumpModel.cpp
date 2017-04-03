a674e19dea745f4b1830d725efcd90208cf1b0e2
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2017-04-03 15:42:47 +0200

    *make more verbose

--- src/meshKernel/bVObserver/bVODumpModel.cpp
+++ src/meshKernel/bVObserver/bVODumpModel.cpp
@@ -11,6 +11,10 @@
 #include <meshEngine/dtGmshModel.h>
 #include <gmsh/Field.h>
 #include <gmsh/Context.h>
+#include <meshEngine/dtGmshVertex.h>
+#include <meshEngine/dtGmshEdge.h>
+#include <meshEngine/dtGmshFace.h>
+#include <meshEngine/dtGmshRegion.h>
 
 namespace dtOO {  
   bVODumpModel::bVODumpModel() {
@@ -46,10 +50,37 @@ namespace dtOO {
 		}
     dt__forAllRefAuto(gm->vertices(), aV) {
       logC() 
-        << logMe::dtFormat("dtGmshVertex[ %3i ]: meshSize = %f")
-          % aV->tag() % aV->prescribedMeshSizeAtVertex() 
+        << logMe::dtFormat("dtGmshVertex[ %3i ]: meshSize = %f, is(dtGmshVertex) = %i")
+          % aV->tag() % aV->prescribedMeshSizeAtVertex() % dtGmshVertex::Is(aV)
         << std::endl;
+    }
+    dt__forAllRefAuto(gm->edges(), aE) {
+      logC() 
+        << logMe::dtFormat("dtGmshEdge[ %3i ]: is(dtGmshEdge) = %i")
+          % aE->tag() % dtGmshEdge::Is(aE) << std::endl
+        << logMe::dtFormat("dtGmshEdge[]: is(getBeginVertex) = %i, is(getEndVertex) = %i")
+          % dtGmshVertex::Is( aE->getBeginVertex() )        
+          % dtGmshVertex::Is( aE->getEndVertex() )        
+        << std::endl;
+      dt__forAllRefAuto( aE->vertices(), aEV ) {
+        logC()
+          << logMe::dtFormat("dtGmshEdge[]: is(vertices[]) = %i")
+            % dtGmshVertex::Is( aEV )
+          << std::endl;
+      }
     }      
+    dt__forAllRefAuto(gm->faces(), aF) {
+      logC() 
+        << logMe::dtFormat("dtGmshFace[ %3i ]: is(dtGmshFace) = %i")
+          % aF->tag() % dtGmshFace::Is(aF)
+        << std::endl;
+    }
+    dt__forAllRefAuto(gm->regions(), aR) {
+      logC() 
+        << logMe::dtFormat("dtGmshRegion[ %3i ]: is(dtGmshRegion) = %i")
+          % aR->tag() % dtGmshRegion::Is(aR)
+        << std::endl;
+    }              
     logC()
       << "Gmsh fieldManager:" << std::endl
       << "size: " << gm->getFields()->size()
