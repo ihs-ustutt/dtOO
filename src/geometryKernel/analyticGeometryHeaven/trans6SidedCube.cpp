129e6d20f8b32d63a7ce3f76b62231bdd98bdb7c
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2017-04-20 11:57:28 +0200

    *add basic reverse and exchange functions

--- src/geometryKernel/analyticGeometryHeaven/trans6SidedCube.cpp
+++ src/geometryKernel/analyticGeometryHeaven/trans6SidedCube.cpp
@@ -3,6 +3,10 @@
 #include <logMe/logMe.h>
 #include <analyticFunctionHeaven/transIntCube.h>
 #include "map2dTo3d.h"
+#include "analyticSurface.h"
+#include <geometryEngine/geoBuilder/bSplineSurface_exchangeSurfaceConstructOCC.h>
+#include <geometryEngine/geoBuilder/geomSurface_surfaceReverseConstructOCC.h>
+#include <geometryEngine/dtSurface.h>
 #include <interfaceHeaven/stringPrimitive.h>
 #include <interfaceHeaven/ptrVectorHandling.h>
 #include "map3dTo3dTransformed.h"
@@ -13,7 +17,8 @@ namespace dtOO {
     
   }
 
-  trans6SidedCube::trans6SidedCube(const trans6SidedCube& orig) : map3dTo3d(orig) {
+  trans6SidedCube::trans6SidedCube(const trans6SidedCube& orig) 
+    : map3dTo3d(orig) {
     _tI.reset( new transIntCube( *(orig._tI.get()) ) );
   }
   
@@ -137,5 +142,332 @@ namespace dtOO {
     else {
       return map3dTo3d::segmentConstW(ww);
     }
+  }
+  
+  vectorHandling< map2dTo3d const * > const & 
+  trans6SidedCube::constRefMap2dTo3d( void ) const {
+    return _tI->getConstRefToMap2dTo3d();
+  }
+  
+  void trans6SidedCube::reverseU( void ) {
+    _tI.reset(
+      new transIntCube(
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[0]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[3],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[2]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[1],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[4]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[5]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get()
+      )
+    );
+  }
+
+  void trans6SidedCube::reverseV( void ) {
+    _tI.reset( 
+      new transIntCube(    
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[0]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[1]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[2]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get(),      
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[3]
+                )->ptrConstDtSurface(),
+                true, 
+                false
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[5],
+        _tI->getConstRefToMap2dTo3d()[4]    
+      )
+    );    
+  }
+
+  void trans6SidedCube::reverseW( void ) {
+    _tI.reset( 
+      new transIntCube(
+        _tI->getConstRefToMap2dTo3d()[2],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[1]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[0],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[3]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[4]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              geomSurface_surfaceReverseConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[5]
+                )->ptrConstDtSurface(),
+                false, 
+                true
+              ).result()
+            ).get()
+          )
+        ).get()    
+      )
+    );    
+  }  
+  
+  void trans6SidedCube::exchangeUV( void ) {
+    _tI.reset( 
+      new transIntCube(    
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[0]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[4],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[2]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+         ).get(),
+        _tI->getConstRefToMap2dTo3d()[5],
+        _tI->getConstRefToMap2dTo3d()[1],
+        _tI->getConstRefToMap2dTo3d()[3]    
+      )
+    );
+  }
+  
+  void trans6SidedCube::exchangeUW( void ) {
+    _tI.reset(     
+      new transIntCube(    
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[3]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[2]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[1]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[0]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[4]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[5]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+         ).get()
+      )
+    );    
+  }
+    
+  void trans6SidedCube::exchangeVW( void ) {
+    _tI.reset(     
+      new transIntCube(    
+        _tI->getConstRefToMap2dTo3d()[5],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[1]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+        ).get(),
+        _tI->getConstRefToMap2dTo3d()[4],
+        dt__pH(analyticSurface)(
+          new analyticSurface(
+            dt__pH(dtSurface)(
+              bSplineSurface_exchangeSurfaceConstructOCC(
+                analyticSurface::MustConstDownCast(
+                  _tI->getConstRefToMap2dTo3d()[3]
+                )->ptrConstDtSurface()
+              ).result()
+            ).get()
+          )
+         ).get(),
+        _tI->getConstRefToMap2dTo3d()[2],   
+        _tI->getConstRefToMap2dTo3d()[0]
+      )
+    );      
   }
 }
--- src/geometryKernel/analyticGeometryHeaven/trans6SidedCube.h
+++ src/geometryKernel/analyticGeometryHeaven/trans6SidedCube.h
@@ -11,35 +11,42 @@ namespace dtOO {
   class dtTransformer;
   
   class trans6SidedCube : public map3dTo3d {
-  public:
-    dt__class(trans6SidedCube, analyticGeometry);   
-    trans6SidedCube();
-    trans6SidedCube(const trans6SidedCube& orig);
-    trans6SidedCube(
-      map2dTo3d const * const aS0, 
-      map2dTo3d const * const aS1,
-      map2dTo3d const * const aS2, 
-      map2dTo3d const * const aS3, 
-      map2dTo3d const * const aS4,
-      map2dTo3d const * const aS5
-    );    
-    virtual trans6SidedCube * create( void ) const;       
-    virtual trans6SidedCube * clone( void ) const;
-    virtual trans6SidedCube * cloneTransformed( 
-      dtTransformer const * const dtT 
-    ) const;     
-    virtual ~trans6SidedCube();
-    virtual bool isClosed( int const & dir) const;
-    virtual float getMin( int const & dir) const;
-    virtual float getMax( int const & dir) const;
-    virtual dtPoint3 getPoint( 
-      float const & uu, float const & vv, float const & ww 
-    ) const;
-    virtual map2dTo3d * segmentConstU( float const & uu ) const;
-    virtual map2dTo3d * segmentConstV( float const & vv ) const;
-    virtual map2dTo3d * segmentConstW( float const & ww ) const;    
-  private:
-    ptrHandling< transIntCube > _tI;
+    public:
+      dt__class(trans6SidedCube, analyticGeometry);   
+      trans6SidedCube();
+      trans6SidedCube(const trans6SidedCube& orig);
+      trans6SidedCube(
+        map2dTo3d const * const aS0, 
+        map2dTo3d const * const aS1,
+        map2dTo3d const * const aS2, 
+        map2dTo3d const * const aS3, 
+        map2dTo3d const * const aS4,
+        map2dTo3d const * const aS5
+      );    
+      virtual trans6SidedCube * create( void ) const;       
+      virtual trans6SidedCube * clone( void ) const;
+      virtual trans6SidedCube * cloneTransformed( 
+        dtTransformer const * const dtT 
+      ) const;     
+      virtual ~trans6SidedCube();
+      virtual bool isClosed( int const & dir) const;
+      virtual float getMin( int const & dir) const;
+      virtual float getMax( int const & dir) const;
+      virtual dtPoint3 getPoint( 
+        float const & uu, float const & vv, float const & ww 
+      ) const;
+      virtual map2dTo3d * segmentConstU( float const & uu ) const;
+      virtual map2dTo3d * segmentConstV( float const & vv ) const;
+      virtual map2dTo3d * segmentConstW( float const & ww ) const;
+      vectorHandling< map2dTo3d const * > const & constRefMap2dTo3d( void ) const;
+      void reverseU( void );
+      void reverseV( void );
+      void reverseW( void );
+      void exchangeUV( void );
+      void exchangeUW( void );
+      void exchangeVW( void );      
+    private:
+      ptrHandling< transIntCube > _tI;
   };
 }
 #endif	/* TRANS6SIDEDCUBE_H */
