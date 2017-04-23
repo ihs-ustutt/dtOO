4b3462f933fe5a38491ec4c9cd0b75dc83315a84
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2017-04-23 11:53:07 +0200

    *add vec3dBiLinear interpolation

---
+++ src/geometryKernel/analyticFunctionHeaven/vec3dBiLinearTwoD.h
@@ -0,0 +1,40 @@
+#ifndef vec3dBiLinearTwoD_H
+#define	vec3dBiLinearTwoD_H
+
+#include "vec3dTwoD.h"
+#include <vector>
+#include <dtLinearAlgebra.h>
+#include <logMe/dtMacros.h>
+
+namespace dtOO {
+  class dtTransformer;
+  
+  class vec3dBiLinearTwoD : public vec3dTwoD {
+  public:
+    dt__class(vec3dBiLinearTwoD, analyticFunction);     
+    vec3dBiLinearTwoD();
+    vec3dBiLinearTwoD(vec3dBiLinearTwoD const & orig);
+    vec3dBiLinearTwoD(
+      dtPoint3 const & p0, dtPoint3 const & p1, 
+      dtPoint3 const & p2, dtPoint3 const & p3
+    );
+    vec3dBiLinearTwoD * clone( void ) const;
+    virtual vec3dBiLinearTwoD * cloneTransformed(
+      dtTransformer const * const dtT 
+    ) const;           
+    vec3dBiLinearTwoD * create( void ) const;    
+    virtual ~vec3dBiLinearTwoD();
+    virtual aFY Y( aFX const & xx ) const;
+    virtual bool closed( int const & dir ) const;    
+  private:
+    dtVector3 _v0;
+    dtVector3 _v1;
+    dtVector3 _v2;
+    dtVector3 _v3;
+    dtPoint3 _p0;
+    dtPoint3 _p1;
+    dtPoint3 _p2;
+    dtPoint3 _p3;    
+  };
+}
+#endif	/* vec3dBiLinearTwoD_H */
