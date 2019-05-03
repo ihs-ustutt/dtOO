8dc6785b0664dcdd8b946381eb8db75fc2c9fdf3
Alexander Tismer <atismer@ihs.uni-stuttgart.de>
2019-05-03 14:52:29 +0200

    *code cleaning

--- src/meshKernel/map3dTo3dGmsh.h
+++ src/meshKernel/map3dTo3dGmsh.h
@@ -10,7 +10,7 @@ namespace dtOO {
   class analyticFunction;
   class analyticGeometry;
   class dtGmshModel;
-  class map3dTo3d;
+//  class map3dTo3d;
    
   class map3dTo3dGmsh : public gmshBoundedVolume {
   public:
