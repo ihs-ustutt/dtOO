15d251403248edbacb3c6fac9ebb55271f868cc9
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2021-06-18 16:59:20 +0200

    Add creating analyticFunction as bSplineSurface bounded by 4 curves

---
+++ src/generalKernel/xmlHeaven/aFXmlBuilder/bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder.cpp
@@ -0,0 +1,93 @@
+#include "bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder.h"
+
+#include <xmlHeaven/aFXmlBuilderFactory.h>
+#include <xmlHeaven/dtXmlParserBase.h>
+#include <analyticFunctionHeaven/analyticFunction.h>
+#include <geometryEngine/dtCurve.h>
+#include <geometryEngine/dtSurface.h>
+#include <analyticFunctionHeaven/vec3dCurveOneD.h>
+#include <analyticFunctionHeaven/vec3dSurfaceTwoD.h>
+#include <geometryEngine/geoBuilder/bSplineSurface_bSplineCurveFillConstructOCC.h>
+#include <geometryEngine/geoBuilder/bSplineSurface_exchangeSurfaceConstructOCC.h>
+#include <logMe/logMe.h>
+#include <interfaceHeaven/ptrHandling.h>
+#include <baseContainerHeaven/baseContainer.h>
+#include <QtXml/QDomElement>
+#include <QtXml/QDomNode>
+
+namespace dtOO {
+  bool bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder::_registrated 
+  =
+  aFXmlBuilderFactory::registrate(
+    dt__tmpPtr(
+      bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder, 
+      new bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder()
+    )
+  );
+  
+  bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder
+    ::bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder() {
+  }
+
+  bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder
+    ::~bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder() {
+
+  }
+
+  void bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder::buildPart(
+		::QDomElement const & toBuildP, 
+		baseContainer * const bC,
+		cVPtrVec const * const cValP, 
+		aFPtrVec const * const depSFunP,
+		aFPtrVec * sFunP 
+	) const {
+    //
+    //check input
+    //
+    bool hasFunction = dtXmlParserBase::hasChild("analyticFunction", toBuildP);
+    
+    if ( hasFunction ) {
+			vectorHandling< dtCurve const * > cc;
+      std::vector< ::QDomElement > elVec 
+			= 
+			dtXmlParserBase::getChildVector("analyticFunction", toBuildP);
+      dt__forAllIndex(elVec, ii) {
+        dt__pH(analyticFunction) aF(
+				  dtXmlParserBase::createAnalyticFunction(
+            &elVec[ii], bC, cValP, depSFunP
+          )
+				);
+				dt__ptrAss(
+					vec3dCurveOneD const * v3d1d,
+					vec3dCurveOneD::ConstDownCast(aF.get())
+				);
+				cc.push_back( v3d1d->ptrDtCurve()->clone() );
+      }
+
+			ptrHandling<dtSurface> dtS( 
+				bSplineSurface_bSplineCurveFillConstructOCC(cc).result()
+			);
+
+      if ( dtXmlParserBase::hasAttribute("orientation", toBuildP) ) {
+        dt__forAllRefAuto(
+          dtXmlParserBase::getAttributeStrVector(
+            "orientation", toBuildP
+          ), 
+          aOrient
+        ) {
+          if (aOrient == "exchange") {
+            dtS.reset( 
+              bSplineSurface_exchangeSurfaceConstructOCC(dtS.get()).result() 
+            );
+          }
+        }
+      }
+      
+      //
+      // create scaCurve2dOneD
+      //
+      sFunP->push_back( new vec3dSurfaceTwoD( dtS.get() ) );			
+    }
+    else dt__throw(buildPart(), << dt__eval(hasFunction) );
+  }
+}
---
+++ src/generalKernel/xmlHeaven/aFXmlBuilder/bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder.h
@@ -0,0 +1,34 @@
+#ifndef bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder_H
+#define	bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder_H
+
+#include <xmlHeaven/aFXmlBuilder.h>
+#include <logMe/dtMacros.h>
+#include <interfaceHeaven/vectorHandling.h>
+
+class QDomElement;
+
+namespace dtOO {
+  class analyticFunction;
+  class constValue;
+  class baseContainer;
+
+  class bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder 
+    : public aFXmlBuilder {
+    public:
+      dt__classOnlyName(bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder);    
+      dt__classSelfCreate(bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder);    
+      bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder();
+      virtual ~bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder();
+      virtual void buildPart(
+        ::QDomElement const & toBuildP, 
+        baseContainer * const bC,
+        cVPtrVec const * const cValP, 
+        aFPtrVec const * const depSFunP,
+        aFPtrVec * sFunP
+      ) const;
+    private: 
+      static bool _registrated;      
+  };
+}
+#endif	/* bSplineSurface_bSplineCurveFillConstructOCCAFXmlBuilder_H */
+
