44df5789ca84ece8f5e073c368759cc20d5ae22f
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2021-12-08 21:50:19 +0100

    Add xml binding of constrainedFilling builder

---
+++ src/generalKernel/xmlHeaven/aFXmlBuilder/bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder.h
@@ -0,0 +1,34 @@
+#ifndef bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder_H
+#define	bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder_H
+
+#include <xmlHeaven/aFXmlBuilder.h>
+#include <logMe/dtMacros.h>
+#include <interfaceHeaven/vectorHandling.h>
+
+class QDomElement;
+
+namespace dtOO {
+  class analyticFunction;
+  class constValue;
+  class baseContainer;
+
+  class bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder 
+    : public aFXmlBuilder {
+    public:
+      dt__classOnlyName(bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder);    
+      dt__classSelfCreate(bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder);    
+      bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder();
+      virtual ~bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder();
+      virtual void buildPart(
+        ::QDomElement const & toBuildP, 
+        baseContainer * const bC,
+        cVPtrVec const * const cValP, 
+        aFPtrVec const * const depSFunP,
+        aFPtrVec * sFunP
+      ) const;
+    private: 
+      static bool _registrated;      
+  };
+}
+#endif	/* bSplineSurface_constrainedFillingConstructOCCAFXmlBuilder_H */
+
