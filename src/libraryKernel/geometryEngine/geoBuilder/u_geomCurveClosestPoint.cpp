417e2ebf269e203a3ac87bd2aa3949bb658319b0
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2021-12-10 20:58:02 +0100

    Add reparam function by minimization

---
+++ src/libraryKernel/geometryEngine/geoBuilder/u_geomCurveClosestPoint.cpp
@@ -0,0 +1,65 @@
+#include "u_geomCurveClosestPoint.h"
+
+#include <interfaceHeaven/staticPropertiesHandler.h>
+#include <geometryEngine/dtCurve.h>
+#include <dtAnalysis.h>
+#include <Math/Functor.h>
+
+namespace dtOO {
+	u_geomCurveClosestPoint
+    ::u_geomCurveClosestPoint(
+      dtCurve const * const dtC, dtPoint3 const & pXYZ
+	) : _dtC(*dtC), _pXYZ(pXYZ) {
+
+    // 
+    // multidimensional minimization
+    //
+    dt__pH(dtMinimizer) min(
+      dtAnalysis::createMinimizer(":Minuit2::kMigrad:")
+    );
+    ::ROOT::Math::Functor toMin(
+      this, &u_geomCurveClosestPoint::F, 1
+    );			
+    min->SetFunction(toMin);          
+    
+    //
+    // set bounds
+    //
+    min->SetVariable( 0, "U", 0.5, 0.01 );
+    min->SetVariableLimits(0, 0., 1.);
+
+    
+    //
+    // minimizer options
+    //        
+    min->SetMaxFunctionCalls(1000000);
+    min->SetMaxIterations(1000);
+    min->SetTolerance( 1.e-8 );			
+    min->SetPrintLevel(
+      staticPropertiesHandler::getInstance()->getOptionInt("root_printLevel") 
+    );
+    
+    //
+    // minimize
+    //
+    min->Minimize();
+    double const * const theRoot = min->X();
+    
+    _closestU = _dtC.u_uPercent( theRoot[0] );
+	}
+
+	u_geomCurveClosestPoint::~u_geomCurveClosestPoint() {
+	}
+	
+	float u_geomCurveClosestPoint::result( void ) {
+		return _closestU;
+	}
+  
+	double u_geomCurveClosestPoint::F(
+    double const * xx
+  ) const {	
+    return dtLinearAlgebra::length( 
+      _dtC.pointPercent(xx[0]) - _pXYZ 
+    );
+	}    
+}
