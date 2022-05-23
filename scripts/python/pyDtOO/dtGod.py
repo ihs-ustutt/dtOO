e75836bd724b10125a8720b6502a9c4bbb8b62b5
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2022-05-23 13:41:12 +0200

    Documenting first classes

--- scripts/python/pyDtOO/dtGod.py
+++ scripts/python/pyDtOO/dtGod.py
@@ -1,20 +1,57 @@
 import logging
 import os
 from pyDtOO.dtDirectory import dtDirectory
+import warnings
+from typing import Union
 
 class dtGod:
-  class __dtGod:
-    def __init__(self, g, rho):
-        logging.info('Create dtGod')
-        self.g_ = g
-        self.rho_ = rho
-  instance = None
+  """
+  Singleton for constants, lock path, ...
 
-  def __init__(self, g=9.81, rho=1000.):
+  Attributes
+  ----------
+
+  instance : __dtGod | None
+
+  """
+  class __dtGod:
+    def __init__(self, g: float, rho: float) -> None:
+      logging.info('Create dtGod')
+      self.g_ = g
+      self.rho_ = rho
+
+  instance: Union[__dtGod, None] = None
+
+  def __init__(self, g: float=9.81, rho: float=1000.0) -> None:
+    """
+
+
+    Parameters
+    ----------
+    g : float, optional
+      DESCRIPTION. The default is 9.81.
+    rho : float, optional
+      DESCRIPTION. The default is 1000.0.
+
+    Returns
+    -------
+    None
+      DESCRIPTION.
+
+    """
     if not dtGod.instance:
       dtGod.instance = dtGod.__dtGod(g, rho)
 
-  def Murder( self ):
+  def Murder( self ) -> None:
+    """
+    Destroy self.instance
+
+
+    Returns
+    -------
+    None.
+
+    """
     logging.info('Murder dtGod')
     dtGod.instance = None
 
@@ -24,7 +61,9 @@ class dtGod:
   def Rho( self ):
     return dtGod.instance.rho_
 
-  def LockPath( self ):
+  def LockPath( self ) -> dtDirectory:
     if "OSLO_LOCK_PATH" not in os.environ:
-      raise RuntimeError("Environment variable OSLO_LOCK_PATH not defined.")
-    return dtDirectory( os.environ['OSLO_LOCK_PATH'] )
+      warnings.warn("Environment variable OSLO_LOCK_PATH not defined.")
+      return dtDirectory("")
+    else:
+      return dtDirectory( os.environ['OSLO_LOCK_PATH'] )
--- scripts/python/pyDtOO/dtPath.py
+++ scripts/python/pyDtOO/dtPath.py
@@ -3,15 +3,15 @@ import os
 
 class dtPath:
   def __init__(self, fn):
-    if fn[-1] == "/":
+    if fn.find("/")>=0:
       logging.warning("Last character of > fn[-1]==/ < -> rstrip")
     self.fn_ = fn.rstrip("/")
 
   def BaseName(self):
     return os.path.basename(self.fn_)
-  
+
   def DirName(self):
     return os.path.dirname(self.fn_)
-  
+
   def FullName(self):
     return self.fn_
