a7a0d3061981a5e9f1cd84ddb267826ca3fdd86a
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2022-09-07 12:08:02 +0200

    Code cleaning

--- src/geometryKernel/constValueHeaven/constValue.h
+++ src/geometryKernel/constValueHeaven/constValue.h
@@ -12,36 +12,36 @@
 
 namespace dtOO {
   class constValue : public labelHandling {
-  public:
-    dt__class(constValue, constValue);
-    constValue( constValue const & orig );
-    virtual ~constValue();
-    static constValue * create(
-      std::string const & type, 
-      std::string const & label, 
-      std::string const & valueStr,
-      bool const loadable
-    );
-    virtual constValue * clone( void ) const = 0;    
-    virtual void dump(void) const = 0;
-    virtual dtReal getValue(void) const;
-    virtual dtReal getValuePercent(void) const;
-    virtual void setValue(dtReal const toSet);
-    virtual void setRange(dtReal const min, dtReal const max);
-    virtual void writeToElement( 
-      ::QDomDocument & doc, ::QDomElement & element
-    ) = 0;
-    virtual dtReal getMin(void) const;
-    virtual dtReal getMax(void) const;
-    virtual bool loadable( void ) const;
-    virtual void resolveConstraint( 
-      cVPtrVec const * const cVArr
-    );
-  protected:
-    constValue();    
-  private:
-    dtReal _value;
-    bool _loadable;
+    public:
+      dt__class(constValue, constValue);
+      constValue( constValue const & orig );
+      virtual ~constValue();
+      static constValue * create(
+        std::string const & type, 
+        std::string const & label, 
+        std::string const & valueStr,
+        bool const loadable
+      );
+      virtual constValue * clone( void ) const = 0;    
+      virtual void dump(void) const = 0;
+      virtual dtReal getValue(void) const;
+      virtual dtReal getValuePercent(void) const;
+      virtual void setValue(dtReal const toSet);
+      virtual void setRange(dtReal const min, dtReal const max);
+      virtual void writeToElement( 
+        ::QDomDocument & doc, ::QDomElement & element
+      ) = 0;
+      virtual dtReal getMin(void) const;
+      virtual dtReal getMax(void) const;
+      virtual bool loadable( void ) const;
+      virtual void resolveConstraint( 
+        cVPtrVec const * const cVArr
+      );
+    protected:
+      constValue();    
+    private:
+      dtReal _value;
+      bool _loadable;
   };
 }
 #endif	/* constValue_H */
