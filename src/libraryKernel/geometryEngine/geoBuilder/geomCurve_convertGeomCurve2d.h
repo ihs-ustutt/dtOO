ba22709d8ea9debcbd8772d4a4f37457ea8021fb
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2022-12-13 15:05:06 +0100

    Code cleaning

--- src/libraryKernel/geometryEngine/geoBuilder/geomCurve_convertGeomCurve2d.h
+++ src/libraryKernel/geometryEngine/geoBuilder/geomCurve_convertGeomCurve2d.h
@@ -12,13 +12,17 @@ namespace dtOO {
   class dtCurve2d;
   
   class geomCurve_convertGeomCurve2d {
-  public:
-    dt__classOnlyName(geomCurve_convertGeomCurve2d);
-    geomCurve_convertGeomCurve2d( dtCurve2d const * const dtC2d, dtPoint3 const & pp, dtVector3 const & vv );
-    virtual ~geomCurve_convertGeomCurve2d();
-    dtCurve * result( void );
-  private:
-    ptrHandling<dtCurve> _dtC;
+    public:
+      dt__classOnlyName(geomCurve_convertGeomCurve2d);
+      geomCurve_convertGeomCurve2d( 
+        dtCurve2d const * const dtC2d, 
+        dtPoint3 const & pp, 
+        dtVector3 const & vv 
+      );
+      virtual ~geomCurve_convertGeomCurve2d();
+      dtCurve * result( void );
+    private:
+      ptrHandling<dtCurve> _dtC;
   };
 }
 #endif	/* GEOMCURVE_CONVERTGEOMCURVE2D_H */
