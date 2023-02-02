cbdfdabead52f1a42f78d706c7d9eea235339566
Alexander Tismer <atismer@betz.ihs.uni-stuttgart.de>
2023-02-02 16:49:05 +0100

    Code cleaning

--- src/geometryKernel/analyticFunctionHeaven/vec2dCurve2dOneD.h
+++ src/geometryKernel/analyticFunctionHeaven/vec2dCurve2dOneD.h
@@ -15,24 +15,24 @@ namespace dtOO {
   class dtTransformer;
   
   class vec2dCurve2dOneD : public vec2dOneD {
-  public:
-    dt__class(vec2dCurve2dOneD, analyticFunction);     
-    vec2dCurve2dOneD();
-    vec2dCurve2dOneD(const vec2dCurve2dOneD& orig);
-    vec2dCurve2dOneD(dtCurve2d const * const dtC2d);
-    virtual vec2dCurve2dOneD * clone( void ) const;
-    virtual vec2dCurve2dOneD * cloneTransformed(
-      dtTransformer const * const dtT 
-    ) const;          
-    virtual vec2dCurve2dOneD * create( void ) const;    
-    virtual ~vec2dCurve2dOneD();
-    using vec2dOneD::Y; // name hiding
-    virtual aFY Y( dtReal const & xx) const;
-    vectorHandling< renderInterface * > getRender( void ) const;
-    dtCurve2d const * ptrDtCurve2d( void ) const;
-    virtual bool closed( dtInt const & dir ) const;
-  private:
-    ptrHandling<dtCurve2d> _dtC2d;
+    public:
+      dt__class(vec2dCurve2dOneD, analyticFunction);     
+      vec2dCurve2dOneD();
+      vec2dCurve2dOneD(const vec2dCurve2dOneD& orig);
+      vec2dCurve2dOneD(dtCurve2d const * const dtC2d);
+      virtual vec2dCurve2dOneD * clone( void ) const;
+      virtual vec2dCurve2dOneD * cloneTransformed(
+        dtTransformer const * const dtT 
+      ) const;          
+      virtual vec2dCurve2dOneD * create( void ) const;    
+      virtual ~vec2dCurve2dOneD();
+      using vec2dOneD::Y; // name hiding
+      virtual aFY Y( dtReal const & xx) const;
+      vectorHandling< renderInterface * > getRender( void ) const;
+      dtCurve2d const * ptrDtCurve2d( void ) const;
+      virtual bool closed( dtInt const & dir ) const;
+    private:
+      ptrHandling<dtCurve2d> _dtC2d;
   };
 }
 #endif	/* VEC2DCURVE2DONED_H */
