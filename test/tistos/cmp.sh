3be48616cb1264d417dff4224f0fa0f2f0f4b962
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2023-03-09 10:33:38 +0100

    Bugfix
      - compressed stream cannot be read from terminal

--- test/tistos/cmp.sh
+++ test/tistos/cmp.sh
@@ -3,10 +3,9 @@
 cWith=$(echo "$1" | sed -e "s%~.*$%.msh%g")
 echo "Compare $1 <-> $cWith"
 #xzcmp $1 $cWith
-diff --expand-tabs --width=150 --suppress-common-lines --side-by-side \
-  <(xzcat $1 | sed -e '1,/Nodes/d' -e '/EndNodes/,$d'  | tail -n+2 | awk '{print $2" "$3" "$4}' | sort -n -k1) \
-  <(cat $cWith | sed -e '1,/Nodes/d' -e '/EndNodes/,$d' | tail -n+2 | awk '{print $2" "$3" "$4}' | sort -n -k1) \
-  > ${1}.diff
+xzcat $1 | sed -e '1,/Nodes/d' -e '/EndNodes/,$d'  | tail -n+2 | awk '{print $2" "$3" "$4}' | sort -n -k1 > ${1}.f1
+cat $cWith | sed -e '1,/Nodes/d' -e '/EndNodes/,$d' | tail -n+2 | awk '{print $2" "$3" "$4}' | sort -n -k1 > ${1}.f2
+diff --expand-tabs --width=150 --suppress-common-lines --side-by-side ${1}.f1 ${1}.f2 > ${1}.diff
 
 if [ -s ${1}.diff ]; then
   # file is not-empty
