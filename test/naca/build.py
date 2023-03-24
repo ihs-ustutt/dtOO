e13af37c56d55eff86dc1723f4faeaa865dc50df
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2023-03-24 16:16:33 +0100

    Bugfix
      - Use SWIG in naca test case

--- test/naca/build.py
+++ test/naca/build.py
@@ -1,13 +1,4 @@
-import sys
-sys.path.append("/home/ati/sandbox/leap~15.2~x86_64/tools")
-
-from libdtOOPython import *
-
-#from mpi4py import MPI
-
-#comm = MPI.COMM_WORLD
-#rank = comm.Get_rank()
-#print('My rank is ',rank)
+from dtOOPythonSWIG import *
 
 logMe.initLog('build.log')
 dtXmlParser.init("machine.xml", "machineSave.xml") 
@@ -21,8 +12,6 @@ bV = labeledVectorHandlingBoundedVolume()
 dC = labeledVectorHandlingDtCase() 
 dP = labeledVectorHandlingDtPlugin() 
 parser.createConstValue(cV)
-#parser.loadStateToConst("K1_1", cV) 
 parser.destroyAndCreate(bC, cV, aF, aG, bV, dC, dP)
 bV.get("gridGmsh").makeGrid()
-#bV.get("ingvrudtout_blkGridMesh").makeGrid()
 #dC.get("ingvrudtout_coupled_of").runCurrentState()
