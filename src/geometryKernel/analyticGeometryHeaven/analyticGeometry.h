6378ddee87566c8062f6534b3b89cc1e5178d73d
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2025-02-28 14:59:05 +0100

    Cleaning
      - Remove threadSafeRoot

--- src/geometryKernel/analyticGeometryHeaven/analyticGeometry.h
+++ src/geometryKernel/analyticGeometryHeaven/analyticGeometry.h
@@ -29,7 +29,6 @@ License
 #include <interfaceHeaven/renderInterface.h>
 #include <interfaceHeaven/vectorHandling.h>
 #include <interfaceHeaven/labeledVectorHandling.h>
-#include <interfaceHeaven/threadSafeRoot.h>
 #include <mainConceptFwd.h>
 
 namespace dtOO { 
@@ -100,7 +99,6 @@ namespace dtOO {
       mutable dtReal _characteristicLength;
       mutable std::pair< dtPoint3, dtPoint3 > _boundingBox;
       mutable dtReal _boundingBoxValue;
-      threadSafeRoot _threadRoot;
   };
   dt__H_addCloneForpVH(analyticGeometry);
 }
