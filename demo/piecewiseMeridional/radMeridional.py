2d8ef68185d537d185e8d9b3937dd31f18c4e7c9
lustauss <lukas-stau@outlook.com>
2026-02-18 10:00:46 +0100

    added meshing of inlet, channel and suction domain changes in the workflow
    meshing of inlet and channel are done with the class
    map3dTo3dGmsh_gridFromChannel. This class is unchanged from the main
    branch
    
    the meshing of the suction channel is done by the class map3dTo3dGmsh_gridFromLayers
    This calss was adapted to set the FaceToPatchrules for the of case
    Herefort the periodic faces used for the meshing of the layers had to be
    untagged after the allocation of the element numbers. The faces were
    retagged trough the bounding boxes of the multiple bounded surfaces of
    the unstructured region
    
    i changed the workflow, so the meshing of the domains is done in
    radMeridional and not in the map3dTo3dGmsh_... classes as before.
    
    furthermore i started to set up the open foam case

--- demo/piecewiseMeridional/radMeridional.py
+++ demo/piecewiseMeridional/radMeridional.py
@@ -21,13 +21,10 @@ import importlib
 class radMeridional:
 
     def __init__(self, data: dict):
+        
+        # constructiong variables for the class from the Config Class
         for key, value in data.items():
             setattr(self, key, value)
-        print(self.d_inlet)
-        print(self.label)
-        #passi
-
-        #importlib.reload(builder)
 
     def create(self,) -> dtOO.dtBundle:
         # Create an empty container
@@ -44,21 +41,6 @@ class radMeridional:
               '"option" : ['
                 '{"name" : "reparamOnFace_precision", "value" : "1.e-05"},'
                 '{"name" : "reparamInVolume_precision","value" : "1.e-05"},'
-                '{"name" : "reparam_internalRestarts", "value" : "10"},'
-                '{"name" : "reparam_restarts", "value" : "10"},'
-                '{"name" : "reparam_restartIncreasePrecision", "value" : "10."},'
-                '{'
-                  '"name" : "reparam_internalRestartDecreasePrecision",'
-                  ' "value" : "0.9"'
-                '},'
-                '{'
-                  '"name" : "reparamOnFace_minimizer", '
-                  '"value" : ":Minuit2::kMigrad:"'
-                '},'		
-                '{'
-                  '"name" : "reparamInVolume_minimizer", '
-                  '"value" : ":Minuit2::kMigrad:"'
-                '},'				
                 '{"name" : "invY_precision", "value" : "1.e-04"},'
                 '{"name" : "xyz_resolution", "value" : "1.e-05"},'
                 '{"name" : "XYZ_resolution", "value" : "1.e-04"},'
@@ -71,9 +53,6 @@ class radMeridional:
                 '{"name" : "geometry_render_resolution_u", "value" : "200"},'
                 '{"name" : "geometry_render_resolution_v", "value" : "200"},'
                 '{"name" : "geometry_render_resolution_w", "value" : "200"},'
-                '{"name" : "root_printLevel", "value" : "0"},'		
-                '{"name" : "root_maxIterations", "value" : "1000"},'
-                '{"name" : "root_maxFunctionCalls", "value" : "1000000"},'
                 '{"name" : "ompNumThreads", "value" : "2"},'
                 '{"name" : "map1dTo3d_deltaPer", "value" : "0.01"},'
                 '{"name" : "map2dTo3d_deltaPer", "value" : "0.01"},'
@@ -99,11 +78,7 @@ class radMeridional:
         dC = container.cptr_dC() 
         dP = container.cptr_dP() 
         
-        #modname = "dtOOPythonApp.builder.analyticGeometryLayers_piecewiseMeridionalRotContour"
-        #module = self.reloadModule(modname)
-        #from dtOOPythonApp.builder import (
-        #    analyticGeometryLayers_piecewiseMeridionalRotContour
-        #)
+        # creating the meridional contour
         modname = "dtOOPythonApp.builder.analyticGeometryLayers_piecewiseMeridionalRotContour"
         module = self.reloadModule(modname)
         radMeridionalContour = module.analyticGeometryLayers_piecewiseMeridionalRotContour( 
@@ -125,8 +100,8 @@ class radMeridional:
         # with:
         # [hub layer lists] = [[3d layer domain], [bool list radius zero]]
 
-        # returns the unstructured region
-        mv = radMeridionalContour.getUnstructuredRegion(self.nBlades)
+        # returns the unstructured region and its surfaces
+        mv, bs = radMeridionalContour.getUnstructuredRegion(self.nBlades)
         
         modname = "dtOOPythonApp.builder.map3dTo3dGmsh_gridFromLayers"
         module = self.reloadModule(modname)
@@ -134,51 +109,172 @@ class radMeridional:
         # module.map3dTo3dGmsh_gridFromLayers(
             # label, layer data, nElementsLayer, firstElementSize, meshSizeSW, meshSizeCIRC, unstructRegion
         # )
-        #importlib.reload(builder.map3dTo3dGmsh_gridFromLayers)
-        layerMesh = module.map3dTo3dGmsh_gridFromLayers(
-                "meshLayers", 
-                layers, 
-                7, 
-                0.01, 
-                0.05, 
-                0.03,  
-                mv
-            )
-        layerMesh.buildLayerMesh()
-        container = layerMesh.buildExtract(container)
+        
+        # creating the mesh of the suction area with wall layers and the unstructured region
+        container = module.map3dTo3dGmsh_gridFromLayers(
+                "meshLayers",       # label 
+                layers,             # layer data
+                7,                  # number of elements in layer
+                0.01,               # first element size in layer
+                0.05,               # element size streamwise
+                0.03,               # element size circumferential
+                mv,                 # multi bounded volume of unstructured region
+                bs                  # surfaces of unstructured region
+            ).buildExtract(container)
+        bV["meshLayers"].makeGrid()
         
         modname = "dtOOPythonApp.builder.map3dTo3dGmsh_gridFromChannel"
         module = self.reloadModule(modname)
         #from dtOOPythonApp.builder import ( map3dTo3dGmsh_gridFromChannel )
-        inletMesh = module.map3dTo3dGmsh_gridFromChannel(
-                "meshInlet",                            # label
-                radMeridionalContour.getRegChannel(0, self.nBlades),    # channel
-                5,                                              # nBoundaryLayersH2S
-                15,                                             # nEHubToShroud
-                20,                                             # nECircumferential
-                25,                                             # nEMeridional
-                0.01,                                           # fESHubToShroud
-                0.01,                                            # fESInlet
-            ).enableDebug()
-        #inletMesh.build()
-        container = inletMesh.buildExtract(container)
 
-        channelMesh = module.map3dTo3dGmsh_gridFromChannel(
-                "channelInlet",                            # label
-                radMeridionalContour.getRegChannel(1, self.nBlades),    # channel
-                5,                                              # nBoundaryLayersH2S
-                15,                                             # nEHubToShroud
-                20,                                             # nECircumferential
-                25,                                             # nEMeridional
-                0.01,                                           # fESHubToShroud
-                0.01,                                            # fESInlet
-            ).enableDebug()
-        #inletMesh.build()
-        container = inletMesh.buildExtract(container)
+        container = module.map3dTo3dGmsh_gridFromChannel(
+                "meshInlet",                                            # label
+                radMeridionalContour.getRegChannel(0, self.nBlades),    # channel
+                5,                                                      # nBoundaryLayersH2S
+                15,                                                     # nEHubToShroud
+                20,                                                     # nECircumferential
+                25,                                                     # nEMeridional
+                0.01,                                                   # fESHubToShroud
+                0.01,                                                   # fESInlet
+            ).buildExtract(container)
+        bV["meshInlet"].makeGrid()
+
+        container = module.map3dTo3dGmsh_gridFromChannel(
+                "meshChannel",                            
+                radMeridionalContour.getRegChannel(1, self.nBlades),
+                5,                                              
+                15,                                             
+                20,                                             
+                25,                                             
+                0.01,                                           
+                0.01,                                           
+            ).buildExtract(container)
+        bV["meshChannel"].makeGrid()
 
         
-        print("A CHANGE IN radMeridional")
+        from dtOOPythonApp.builder import (
+          ofOpenFOAMCase_turboMachine,
+          ofOpenFOAMCase_setupWrapper
+        )
 
+        container = ofOpenFOAMCase_turboMachine(
+          label = "of",
+          bVs = [
+            bV["meshInlet"], bV["meshChannel"], bV["meshLayers"],
+          ],
+          dictRule = \
+              ofOpenFOAMCase_setupWrapper.controlDict(
+                application = "simpleFoam",
+                endTime = 200,
+                QPatches = ['meshInlet_inlet','meshLayers_outlet',],
+                PTPatches = ['meshInlet_inlet', 'meshLayers_outlet',],
+                #FPatches = ['mesh_blade',],
+                libs = [
+                  "libsimpleFunctionObjects.so",
+                  "libsimpleSwakFunctionObjects.so",
+                  "libmappedFieldFixedValue.so",
+                  "libmixingPlane.so",
+                ]
+              )
+            + ofOpenFOAMCase_setupWrapper.fvSchemes()
+            + ofOpenFOAMCase_setupWrapper.fvSolution()
+            + ofOpenFOAMCase_setupWrapper.transportModel()
+            + ofOpenFOAMCase_setupWrapper.turbulenceProperties(),
+            #+ ofOpenFOAMCase_setupWrapper.MRFProperties(
+            #    cellZones = ["mesh",],
+            #    omegas    = [9.81,],
+            #    nonRotatingPatches = [
+            #      [
+            #        "mesh_suction", "mesh_pressure",
+            #        "mesh_inlet", "mesh_outlet",
+            #        "mesh_shroud",
+            #      ],
+            #    ],
+            #    patches = [],
+            #    axes = [dtOO.dtVector3(0,0,1)],
+            #    origins = [dtOO.dtPoint3(0,0,0)]
+            #  ),
+            fieldRules = [
+              ofOpenFOAMCase_setupWrapper.fieldRuleString("U", [0.0,0.0,-1.0,]),
+              ofOpenFOAMCase_setupWrapper.fieldRuleString("p", [0.0,]),
+              ofOpenFOAMCase_setupWrapper.fieldRuleString("k", [0.1,]),
+              ofOpenFOAMCase_setupWrapper.fieldRuleString("omega", [0.1,]),
+              ofOpenFOAMCase_setupWrapper.fieldRuleString("nut", [0.1,]),
+            ],
+            setupRules = [
+              ofOpenFOAMCase_setupWrapper.emptyRuleString(),
+              # meshInlet
+              ofOpenFOAMCase_setupWrapper.cylindricalInletRuleString(
+                "meshInlet_inlet",
+                ["U"],
+                [ [-2,0,0], ]
+              ),
+              ofOpenFOAMCase_setupWrapper.inletRuleString(
+                "meshInlet_inlet",
+                ["p", "k", "omega",],
+                [ [0], [0.0, 0.10], [0.001, 0.1] ]
+              ),
+              ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshInlet_shroud",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+               ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshInlet_hub",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+              #ofOpenFOAMCase_setupWrapper.wallRuleString(
+              #  "mesh_blade",
+              #  ["omega", "U", "p", "k", "nut"]
+              #),
+              ofOpenFOAMCase_setupWrapper.cyclicAmiRuleString(
+                "meshInlet_suction", "meshInlet_pressure"
+              ),
+              # mixing plane?
+              ofOpenFOAMCase_setupWrapper.mixingPlaneRuleString(
+                "meshInlet_outlet", "meshChannel_inlet",
+                ["U", "p", "k", "omega",],
+                axis = dtOO.dtVector3(0,0,1),
+                origin = dtOO.dtPoint3(0,0,0),
+                stackAxis = "R"
+              ),
+              # mesh Channel
+              ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshChannel_hub",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+              ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshChannel_shroud",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+              ofOpenFOAMCase_setupWrapper.cyclicAmiRuleString(
+                "meshChannel_suction", "meshChannel_pressure"
+              ),
+              ofOpenFOAMCase_setupWrapper.mixingPlaneRuleString(
+                "meshChannel_outlet", "meshLayers_inlet",
+                ["U", "p", "k", "omega",],
+                axis = dtOO.dtVector3(0,0,1),
+                origin = dtOO.dtPoint3(0,0,0),
+                stackAxis = "R"
+              ),
+              ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshLayers_hub",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+              ofOpenFOAMCase_setupWrapper.wallRuleString(
+                "meshLayers_shroud",
+                ["omega", "U", "p", "k", "nut"]
+              ),
+              ofOpenFOAMCase_setupWrapper.cyclicAmiRuleString(
+                "meshLayers_periodic0", "meshLayers_periodic1"
+              ),
+              ofOpenFOAMCase_setupWrapper.outletRuleString(
+                "meshLayers_outlet", 
+                ["U", "p", "k", "omega",]
+              ),
+            ]
+
+        ).buildExtract( container )
+        
         return container  
 
     def reloadModule(self, modname):  
