428274f2fec00be3884c45c2dd224351e5016fdb
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2026-02-24 11:25:27 +0100

    Enhancement
      - Add builder to sort edge loop

---
+++ src/geometryKernel/analyticGeometryHeaven/aGBuilder/map1dTo3d_orderLoop.h
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------*\
+  dtOO < design tool Object-Oriented >
+
+    Copyright (C) 2024 A. Tismer.
+-------------------------------------------------------------------------------
+License
+    This file is part of dtOO.
+
+    dtOO is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the LICENSE.txt file in the
+    dtOO root directory for more details.
+
+    You should have received a copy of the License along with dtOO.
+
+\*---------------------------------------------------------------------------*/
+
+#ifndef map1dTo3d_orderLoop_H
+#define map1dTo3d_orderLoop_H
+
+#include <dtOOTypeDef.h>
+
+#include <logMe/dtMacros.h>
+
+namespace dtOO {
+class map1dTo3d;
+
+class map1dTo3d_orderLoop {
+public:
+  dt__classOnlyName(map1dTo3d_orderLoop);
+  map1dTo3d_orderLoop(dt__pVH(map1dTo3d) const &m1ds);
+  virtual ~map1dTo3d_orderLoop();
+  dt__pVH(map1dTo3d) result(void);
+
+private:
+  dt__pVH(map1dTo3d) _m1ds;
+};
+} // namespace dtOO
+#endif /* map1dTo3d_orderLoop_H */
