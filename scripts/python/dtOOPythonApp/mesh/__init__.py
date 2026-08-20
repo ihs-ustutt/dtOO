632ba9f0898b22158c4edee06469df66285187b7
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2026-08-20 12:06:22 +0200

    Improvement
      - Create a `robustCompare` class to compare meshes in a robust way;
        e.g. compare number of hexahedrons instead of directly compare the
        node positions
    
      - Modify test's `CMakeLists.txt` to search for files containing `*cmp*`
        wildcard; all `cmp`-files are then applied to all `*.xz`-files,
        except the cmp file is targeted to a special `xz`-file with a string
        in the `cmp`'s filename
    
      - Update ThirdParty directory to have new `meshio` dependendcy
        installed, see ihs-ustutt/dtoo-thirdparty#21
    
      - Update `xz`-files of tistos

--- scripts/python/dtOOPythonApp/mesh/__init__.py
+++ scripts/python/dtOOPythonApp/mesh/__init__.py
@@ -15,35 +15,6 @@
 #
 #------------------------------------------------------------------------------
 
-"""Package for creating hydraulic machinery
-
-# What is dtOOPythonApp?
-
-The package contains classes for creating hydraulic machinery. It is split in
-three subpackages:
-
-  - builder: main builders for machine creation
-
-  - tools: tools used by builders
-
-  - vis: classes for visualization
-
-  - mesh: class for mesh handling
-
-# Quickstart
-
-The whole package is imported by:
-
-```python
-import dtOOPythonApp as dpa
-```
-"""
-from .builder import *
-from .tools import *
-
-import importlib.util
-if importlib.util.find_spec("paraview") is not None:
-  if importlib.util.find_spec("paraview.simple") is not None:
-    from .vis import *
-
-from .mesh import *
+from .robustCompare import (
+  robustCompare
+)
