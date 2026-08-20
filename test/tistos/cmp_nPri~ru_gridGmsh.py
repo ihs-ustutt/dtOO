c1bce85e6c1b59e04ff6bc0405bbba8a2cf250f8
Alexander Tismer <alexander.tismer@ihs.uni-stuttgart.de>
2026-08-20 15:29:22 +0200

    Update and remove
      - Remove `cmp.sh` from tistos
    
      - Add two new tests

---
+++ test/tistos/cmp_nPri~ru_gridGmsh.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import logging
+logging.basicConfig(
+  format='[ %(asctime)s - %(levelname)8s - %(filename)s:%(lineno)d ]'
+         ' - %(message)s', 
+  datefmt='%d-%b-%y %H:%M:%S', 
+  level=logging.INFO
+)
+import sys
+import re
+from dtOOPythonApp.mesh import robustCompare
+
+new_mesh = sys.argv[1]
+org_mesh = re.sub(r"~.*\.msh.*$", ".msh", new_mesh)
+
+if robustCompare(org_mesh, new_mesh).compareElementNumber("MPrism"):
+    print("OK")
+    sys.exit(0)
+
+sys.exit(1)
